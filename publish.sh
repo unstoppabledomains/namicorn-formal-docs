5daf04a76d8b35574747081132eea59c8e81d4eb
Johnny <johnny@unstoppabledomains.com>
2019-10-30 00:44:45 -0700

    updated version

--- publish.sh
+++ publish.sh
@@ -1,10 +1,11 @@
 # // To publish we want to copy all files from the last version to a folder /docs. Before we do such we need to delete /docs
 echo $1
 # // Delete /docs/*
-sudo rm -rf ./docs/*
+rm -rf ./docs/*
 # // Copy eveything from $1 to /docs
 mkdir -p ./docs
-sudo cp -r $1 ./docs
+echo $1
+cp -r $1 ./docs
 # // run git commands to push the changes
 echo $PWD
 # git commit -m "$1"
